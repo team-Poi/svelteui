69c41cf45b3fb620ac5f4dc2828b174c139b3a84
Oein <oein0219@gmail.com>
2023-08-27 19:27:29 +0900

    Add Dev Main Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import ColorViewer from '@/lib/ThemeProvider/ColorViewer.svelte';
+	import { Progress, Container, Card, Button } from '@/lib';
+	let pro = 0.5;
+</script>
+
+<Container>
+	<ColorViewer />
+	<div style="width: 100%; box-sizing: border-box;">
+		<Card Layer="2" style="width: 100%; box-sizing: border-box; margin-top: 1rem;">
+			<div>PRIMARY</div>
+			<Progress progress={pro} size="SMALL" color="PRIMARY" />
+			<div>SECONDARY</div>
+			<Progress progress={pro} size="NORMAL" color="SECONDARY" />
+			<div>SUCCESS</div>
+			<Progress progress={pro} size="BIG" color="SUCCESS" />
+			<div>INFO</div>
+			<Progress progress={pro} size="SMALL" color="INFO" />
+			<div>ERROR</div>
+			<Progress progress={pro} size="NORMAL" color="ERROR" />
+			<div>WARNING</div>
+			<Progress progress={pro} size="BIG" color="WARNING" />
+		</Card>
+	</div>
+	<Card Layer="2" style="width: 100%; box-sizing: border-box; margin-top: 1rem;">
+		<div>PRIMARY</div>
+		<Button color="PRIMARY">PRIMARY</Button>
+		<div>SECONDARY</div>
+		<Button color="SECONDARY">SECONDARY</Button>
+		<div>SUCCESS</div>
+		<Button color="SUCCESS">SUCCESS</Button>
+		<div>INFO</div>
+		<Button color="INFO">INFO</Button>
+		<div>ERROR</div>
+		<Button color="ERROR">ERROR</Button>
+		<div>WARNING</div>
+		<Button color="WARNING">WARNING</Button>
+	</Card>
+</Container>
