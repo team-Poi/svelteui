a176ad06bf0a172a90562164fe9e82e5debf2b1b
Oein <oein0219@gmail.com>
2023-08-27 19:17:46 +0900

    as Container Component

---
+++ src/lib/Container/Container.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	export let background = 'none';
+	export let style = '';
+</script>
+
+<div class="wrapper" style:background>
+	<div class="container" style:background {style}>
+		<slot />
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		width: 100%;
+		height: fit-content;
+		box-sizing: border-box;
+	}
+	.container {
+		padding-right: 15px;
+		padding-left: 15px;
+		margin-right: auto;
+		margin-left: auto;
+	}
+
+	@media (min-width: 768px) {
+		.container {
+			width: 750px;
+		}
+	}
+
+	@media (min-width: 992px) {
+		.container {
+			width: 970px;
+		}
+	}
+
+	@media (min-width: 1200px) {
+		.container {
+			width: 1170px;
+		}
+	}
+</style>
