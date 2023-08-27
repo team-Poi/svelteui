f811aefa723105171be3af3fbca4eaf4be132de8
Oein <oein0219@gmail.com>
2023-08-27 19:26:47 +0900

    Add Card Component

---
+++ src/lib/Card/Card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let Layer: '1' | '2' | '3';
+	export let style = '';
+
+	let COLOR = 'FIRST';
+	if (Layer == '2') COLOR = 'SECOND';
+	if (Layer == '3') COLOR = 'THIRD';
+</script>
+
+<div class={'card'} style={`--color: var(--LAYER_BACKGROUND-${COLOR}); ${style}`}>
+	<slot />
+</div>
+
+<style>
+	.card {
+		padding: 8px;
+		border-radius: var(--RADIUS-NORMAL);
+		box-shadow: var(--SHADOW-LIGHT);
+		width: fit-content;
+		height: fit-content;
+		display: inline-block;
+
+		background: var(--color);
+		transition: none;
+	}
+</style>
