26d8656c30b357033a581a7703abfe808205dee4
Oein <oein0219@gmail.com>
2023-10-01 21:02:18 +0900

    test page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
 	import ColorViewer from '@/lib/ThemeProvider/ColorViewer.svelte';
 	import { Progress, Container, Card, Button, Input } from '@/lib';
+	import Textarea from '@/lib/Textarea';
+	import _0x53f95b from './encrypt.js';
+	import Checkbox from '@/lib/Checkbox/Checkbox.svelte';
 	import Loading from '@/lib/Loading/Loading.svelte';
 	let pro = 0.5;
+
+	let a = '';
+	let b = '';
+	let c = '';
+
+	$: {
+	}
 </script>
 
 <Container>
+	<div style="padding-top: var(--GAP-MAIN);" />
+	<Card
+		style="width: 100%; box-sizing: border-box; display: flex; flex-direction: column; flex-wrap: wrap; gap: var(--GAP-HALF); "
+	>
+		<Textarea placeholder="Input" bind:value={a} />
+		<Input placeholder="API Endpoint" bind:value={b} />
+		<Textarea placeholder="Result" readonly bind:value={c} />
+	</Card>
+
 	<ColorViewer />
 	<div style="width: 100%; box-sizing: border-box;">
-		<Card Layer="2" style="width: 100%; box-sizing: border-box; margin-top: 1rem;">
+		<Card style="width: 100%; box-sizing: border-box; margin-top: var(--GAP-MAIN);">
 			<div>PRIMARY</div>
 			<Progress progress={pro} size="SMALL" color="PRIMARY" />
 			<div>SECONDARY</div>
@@ -23,7 +42,7 @@
 			<Progress progress={pro} size="BIG" color="WARNING" />
 		</Card>
 	</div>
-	<Card Layer="2" style="width: 100%; box-sizing: border-box; margin-top: 1rem;">
+	<Card style="width: 100%; box-sizing: border-box; margin-top: var(--GAP-MAIN);">
 		<div>PRIMARY</div>
 		<Button color="PRIMARY">PRIMARY</Button>
 		<div>SECONDARY</div>
@@ -38,7 +57,10 @@
 		<Button color="WARNING">WARNING</Button>
 	</Card>
 	<div style="width: 100%; box-sizing: border-box;">
-		<Card Layer="2" style="width: 100%; box-sizing: border-box; margin-top: 1rem;">
+		<Card
+			style="width: 100%; box-sizing: border-box; margin-top: var(--GAP-MAIN); display: flex; flex-direction: column; gap: var(--GAP-HALF)"
+		>
+			<Input placeholder="NORMAL" />
 			<Input color="PRIMARY" placeholder="PRIMARY" />
 			<Input color="SECONDARY" placeholder="SECONDARY" />
 			<Input color="SUCCESS" placeholder="SUCCESS" />
@@ -47,5 +69,28 @@
 			<Input color="WARNING" placeholder="WARNING" />
 		</Card>
 	</div>
-	<Loading size="8rem" />
+
+	<Card
+		style="width: 100%; box-sizing: border-box; margin-top: var(--GAP-MAIN); display: flex; flex-wrap: wrap; gap: var(--GAP-HALF); justify-content: center;"
+	>
+		<Loading size="8rem" color="PRIMARY" />
+		<Loading size="8rem" color="SECONDARY" />
+		<Loading size="8rem" color="SUCCESS" />
+		<Loading size="8rem" color="INFO" />
+		<Loading size="8rem" color="ERROR" />
+		<Loading size="8rem" color="WARNING" />
+	</Card>
+	<Card
+		style="width: 100%; box-sizing: border-box; margin-top: var(--GAP-MAIN); display: flex; flex-wrap: wrap; gap: var(--GAP-HALF); justify-content: center;"
+	>
+		<Checkbox color="PRIMARY" />
+		<Checkbox color="SECONDARY" />
+		<Checkbox color="SUCCESS" />
+		<Checkbox color="INFO" />
+		<Checkbox color="ERROR" />
+		<Checkbox color="WARNING" />
+	</Card>
+	<Card
+		style="width: 100%; box-sizing: border-box; margin-top: var(--GAP-MAIN); display: flex; flex-wrap: wrap; gap: var(--GAP-HALF); justify-content: center;"
+	/>
 </Container>
