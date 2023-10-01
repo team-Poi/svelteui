abb3fc1417df23fb4a9d257181ce3fa6732fc735
Oein <oein0219@gmail.com>
2023-10-01 21:02:58 +0900

    change style

--- src/lib/Card/Card.svelte
+++ src/lib/Card/Card.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
-	export let Layer: '1' | '2' | '3';
 	export let style = '';
-
-	let COLOR = 'FIRST';
-	if (Layer == '2') COLOR = 'SECOND';
-	if (Layer == '3') COLOR = 'THIRD';
 </script>
 
-<div class={'card'} style={`--color: var(--LAYER_BACKGROUND-${COLOR}); ${style}`}>
+<div class={'card'} style={`--color: var(--LAYER_BACKGROUND); ${style}`}>
 	<slot />
 </div>
 
 <style>
 	.card {
-		padding: 8px;
+		padding: var(--GAP-HALF);
 		border-radius: var(--RADIUS-NORMAL);
 		box-shadow: var(--SHADOW-LIGHT);
 		width: fit-content;
